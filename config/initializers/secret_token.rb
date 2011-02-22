0c199f0aaadc76de8d16b08681b9e0c1ac060f50
William Hamilton-Levi <whamilt1@swarthmore.edu>
2011-02-22 14:41:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KitsuneUdon::Application.config.secret_token = '307b4e2ed03634daada3d43f1cd5f8ea4677d7259fed2c90395b43e7a878fb796759a710008c43f015a5679ce5276439e75992aa708bf08a54d98f8ab9042748'
