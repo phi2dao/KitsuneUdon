3a3207effb4465ede6be7887065b3d3305b1656f
William Hamilton-Levi <whamilt1@swarthmore.edu>
2011-02-22 17:22:32 -0500

    Working Version

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,21 @@
 module PagesHelper
+	def title
+		if @title
+			"KitsuneUdon | #{@title}"
+		else
+			"KitsuenUdon"
+		end
+	end
+	
+	def lang_source
+		if params.key? :data
+			params[:data][:source]
+		end
+	end
+	
+	def lang_times
+		if params.key? :data
+			params[:data][:times]
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ KitsuneUdon::Application.routes.draw do
 	
 	match '/home', :to => 'pages#home'
 	get "pages/home"
+	root :to => 'pages#home'
 	
 	match '/help', :to => 'pages#help'
 	get "pages/help"
 	
 	match '/output', :to => 'pages#output'
 	get "pages/output"
-	
-	root :to => 'pages#home'
 end
